3e9f077db1a6ee80184d1305393d07a9d8c6e362
child <firewalla.zhijie@gmail.com>
2023-09-18 15:06:06 +0800

    update

---
+++ get-active-alarms-by-specific-box/README.md
@@ -0,0 +1,18 @@
+### Example
+
+Get active alarms by specific box.
+
+
+### Quick Start
+
+Assume you've already cloned `https://github.com/firewalla/msp-api-examples.git` and `cd msp-api-examples`
+```bash
+cd get-active-alarms-by-specific-box
+npm install
+domain="<YOUR-MSP-DOMAIN>" token="<YOUR-MSP-TOKEN>" node ./index.js
+
+```
+
+### Dependencies
+- [Node.js](https://nodejs.org/)
+- [npm](https://www.npmjs.com/package/npm), [pnpm](https://pnpm.io/installation), or the package manager you prefer
---
+++ get-active-alarms-by-specific-box/index.js
@@ -0,0 +1,67 @@
+/*    Copyright 2023 Firewalla Inc.
+ *  
+ *    MIT License
+
+ *    Permission is hereby granted, free of charge, to any person obtaining
+ *    a copy of this software and associated documentation files (the
+ *    "Software"), to deal in the Software without restriction, including
+ *    without limitation the rights to use, copy, modify, merge, publish,
+ *    distribute, sublicense, and/or sell copies of the Software, and to
+ *    permit persons to whom the Software is furnished to do so, subject to
+ *    the following conditions:
+
+ *    The above copyright notice and this permission notice shall be
+ *    included in all copies or substantial portions of the Software.
+
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import axios from 'axios';
+import fs from 'fs';
+
+// Create .token and .domain file or use environment variables to setup your MSP domain and credential
+const msp_domain = process.env.domain || fs.readFileSync('./.domain').toString();
+const token = process.env.token || fs.readFileSync('./.token').toString();
+const box = process.env.box;
+
+
+// Related API Document
+// https://docs.firewalla.net/api-reference/device/
+async function main() {
+
+    const httpClient = axios.create({
+        baseURL: `https://${msp_domain}/v2`,
+    })
+    httpClient.defaults.headers.common['Authorization'] = 'Token ' + token;
+    httpClient.defaults.headers.common['Content-Type'] = 'application/json'
+
+    const params = {
+        query: `status:active box:${box}`,
+        cursor: null,
+        limit: 10
+    }
+    const alarms = [];
+    while (1) {
+        const { results, next_cursor } = await httpClient({
+            method: 'get',
+            url: `/alarms`,
+            params: params
+        }).then(r => r.data);
+        alarms.push(...results);
+        if (!next_cursor) break;
+        params.cursor = next_cursor;
+    }
+    console.table(alarms, ['message']);
+}
+
+
+main().catch(err => {
+    console.error('Failed to get alarms', err);
+    process.exit(1)
+})
